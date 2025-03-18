7f53da5c73ef44f581902112530cf62e6a73a64b
Tio Bagio <10209203+tiobagio@users.noreply.github.com>
2025-03-18 19:55:50 +0800

    Update outputs.tf
    change "cat" to "bank"

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 # Outputs file
 
-output "catapp_ip" {
-  value = "http://${aws_eip.hashicat.public_ip}"
+output "bankapp_ip" {
+  value = "http://${aws_eip.hashibank.public_ip}"
 }
 
 output "private_key" {
-  value = tls_private_key.hashicat.private_key_pem
+  value = tls_private_key.hashibank.private_key_pem
   sensitive = true
 }
-output "catapp_url" {
-  value = "http://${aws_eip.hashicat.public_dns}"
+output "bankapp_url" {
+  value = "http://${aws_eip.hashibank.public_dns}"
 }
